c945ede82c30cc5048d675e6d984c5631b049a48
subodhmyriad <subodh@myriad-tech.com>
2023-01-26 19:11:38 +0530

    commit

--- dj-event/dj-back-end/src/api/event/models/events.js
+++ dj-event/dj-back-end/src/api/event/models/events.js
@@ -2,17 +2,17 @@
 const slugify = require("slugify");
 
 module.exports = {
-    beforeCreate(event) {
-        const { data } = event.params;
-        if (data.name) {
-            data.slug = slugify(data.name, { lower: true });
-        }
-    },
-    beforeUpdate(event) {
-        const { data } = event.params;
-        if (data.name) {
-            data.slug = slugify(data.name, { lower: true });
-        }
+    lifecycles: {
+        beforeCreate: async (data) => {
+            if (data.name) {
+                data.slug = slugify(data.name, { lower: true });
+            }
+        },
+        beforeUpdate: async (params, data) => {
+            if (data.name) {
+                data.slug = slugify(data.name, { lower: true });
+            }
+        },
     },
 };
 
